1d3980ae21bf2b91c90b1fd5e40660a0b436e2ba
Samir Khoury <samir@khoury.tk>
2022-05-15 10:18:59 +0300

    react router

--- side-by-side/src/App.js
+++ side-by-side/src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter as Router, Route,  Routes } from "react-router-dom";
 
+import Homepage from "./components/homepage/Homepage";
+import Navbar from './components/navbar/Navbar'
+import Update from './components/updates/Updates'
+import About from "./components/about/About";
+import CouponPopup from "./components/coupon/CouponPopup";
+import Login from "./components/login/Login";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Homepage />} />
+          <Route exact path="/update" element={<Update />} />
+          <Route exact path="/about" element={<About />} />
+          <Route exact path="/popup" element={<CouponPopup />} />
+          <Route exact path="/login" element={<Login />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
