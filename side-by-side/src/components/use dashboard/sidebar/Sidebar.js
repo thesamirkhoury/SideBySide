49987313978b86513e2593b5ac6b38e98906c974
JENNIFER <jennifer.shammas.2000@gmail.com>
2022-06-07 13:17:16 +0300

    sidebar bugfix

--- side-by-side/src/components/use dashboard/sidebar/Sidebar.js
+++ side-by-side/src/components/use dashboard/sidebar/Sidebar.js
@@ -3,16 +3,17 @@ import "./Sidebar.css";
 import Lecture from "../assests/Lecture.svg";
 import lectureFilled from "../assests/LectureFilled.svg";
 import Coupon from "../assests/Coupons-icon.svg";
-import CouponFilled from '../assests/CouponFilled.svg'
+import CouponFilled from "../assests/CouponFilled.svg";
 import Ticket from "../assests/Tickets-icon.svg";
-import TicketFilled from '../assests/TicketsFilled.svg'
+import TicketFilled from "../assests/TicketsFilled.svg";
 import Setting from "../assests/Settings-icon.svg";
-import SettingFilled from '../assests/SettingFilled.svg'
+import SettingFilled from "../assests/SettingFilled.svg";
 import Logout from "../assests/Logout-icon.svg";
 import { NavLink } from "react-router-dom";
-import {useUserContext} from '../../context/UseContext'
+import { useUserContext } from "../../context/UseContext";
 function Sidebar() {
-  const { signInUser, userLogged,setUserLogged, wrongCreds } = useUserContext();
+  const { signInUser, userLogged, setUserLogged, wrongCreds } =
+    useUserContext();
 
   const [lectImgFilled, setLectImgFilled] = React.useState(false);
   const [coupImgFilled, setCouptImgFilled] = React.useState(false);
@@ -29,9 +30,11 @@ function Sidebar() {
           }
         >
           <div className="single-item">
-            <p style={{fontWeight:`${lectImgFilled ? 'bold': ''}`}}>סדנאות</p>
+            <p style={{ fontWeight: `${lectImgFilled ? "bold" : ""}` }}>
+              סדנאות
+            </p>
             <img
-              src={lectImgFilled ? lectureFilled : Lecture} 
+              src={lectImgFilled ? lectureFilled : Lecture}
               alt="icon"
               height="43"
               width="32"
@@ -40,12 +43,14 @@ function Sidebar() {
         </NavLink>
         <NavLink
           to="/userdashboard/coupons"
-          className={({ isActive }) => 
-          isActive ? setCouptImgFilled(true) : setCouptImgFilled(false)
-        }
+          className={({ isActive }) =>
+            isActive ? setCouptImgFilled(true) : setCouptImgFilled(false)
+          }
         >
           <div className="single-item">
-            <p style={{fontWeight:`${coupImgFilled ? 'bold': ''}`}}>הטבות</p>
+            <p style={{ fontWeight: `${coupImgFilled ? "bold" : ""}` }}>
+              הטבות
+            </p>
             <img
               src={coupImgFilled ? CouponFilled : Coupon}
               // className={imgColor}
@@ -57,12 +62,14 @@ function Sidebar() {
         </NavLink>
         <NavLink
           to="/userdashboard/tickets"
-          className={({ isActive }) =>  isActive ? setTickImgFilled(true) : setTickImgFilled(false)}
+          className={({ isActive }) =>
+            isActive ? setTickImgFilled(true) : setTickImgFilled(false)
+          }
         >
           <div className="single-item">
-            <p style={{fontWeight:`${tickmgFilled ? 'bold': ''}`}}>פניות</p>
+            <p style={{ fontWeight: `${tickmgFilled ? "bold" : ""}` }}>פניות</p>
             <img
-              src={tickmgFilled?TicketFilled:Ticket}
+              src={tickmgFilled ? TicketFilled : Ticket}
               style={{ color: "red" }}
               // className={imgColor}
               alt="icon"
@@ -76,12 +83,16 @@ function Sidebar() {
       <div className="lower-items">
         <NavLink
           to="/userdashboard/settings"
-          className={({ isActive }) =>  isActive ? setSetImgFilled(true) : setSetImgFilled(false)}
+          className={({ isActive }) =>
+            isActive ? setSetImgFilled(true) : setSetImgFilled(false)
+          }
         >
           <div className="single-item">
-            <p style={{fontWeight:`${setImgFilled ? 'bold': ''}`}}>הגדרות</p>
+            <p style={{ fontWeight: `${setImgFilled ? "bold" : ""}` }}>
+              הגדרות
+            </p>
             <img
-              src={setImgFilled? SettingFilled :Setting}
+              src={setImgFilled ? SettingFilled : Setting}
               // className={imgColor}
               alt="icon"
               height="43"
@@ -90,7 +101,13 @@ function Sidebar() {
           </div>
         </NavLink>
         <NavLink to="/">
-          <div className="single-item" >
+          <div
+            className="single-item"
+            onClick={() => {
+              setUserLogged(false);
+              localStorage.clear()
+            }}
+          >
             <p>התנתק</p>
             <img src={Logout} alt="icon" height="43" width="32" />
           </div>
